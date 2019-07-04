ad2af6a8d6082be52dbebc74aaa24070eb0dbc65
Асель Молдашева <@lcoact.itstep>
2019-07-04 21:16:43 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Number
+    {
+        private int number;
+        public void SetNumber(int number) { this.number = number; }
+        public static Number operator +(Number obj1, Number obj2)
+        {
+            Number arr = new Number();
+            arr.number = obj1.number + obj2.number;
+            return arr;
+        }
+        public static bool operator ==(Number obj1, Number obj2)
+        {
+            if (obj1.number == obj2.number) return true;
+            else return false;
+        }
+
+        public static bool operator !=(Number obj1, Number obj2)
+        {
+            return !(obj1 == obj2);
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number object1 = new Number();
+            Number object2 = new Number();
+            object1.SetNumber(5);
+            object2.SetNumber(17);
+            Console.WriteLine(object1 == object2);
+            Console.ReadKey();
+
+        }
+    }
+}
